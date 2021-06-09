e348c5e6074b28e483591ec22e08eef472f3c5c0
Beata <20beata17@gmail.com>
2021-06-09 16:50:51 +0200

    labels in coco format - validation

--- labelme2coco.py
+++ labelme2coco.py
@@ -2,10 +2,12 @@
 import labelme2coco
 
 # set directory that contains labelme annotations and image files
-labelme_folder = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/train"
+# labelme_folder = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/train"
+labelme_folder = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/validation"
 
 # set path for coco json to be saved
-save_json_path = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/coco_train"
+# save_json_path = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/coco_train"
+save_json_path = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/coco_val"
 
 # convert labelme annotations to coco
 labelme2coco.convert(labelme_folder, save_json_path)
