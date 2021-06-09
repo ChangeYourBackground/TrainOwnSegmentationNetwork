6b07b0fdf2ef13a9936582d6afcc571fd63a0e85
Beata <20beata17@gmail.com>
2021-06-09 18:14:02 +0200

    Custom train MaskRCNN added

--- labelme2coco.py
+++ labelme2coco.py
@@ -2,12 +2,12 @@
 import labelme2coco
 
 # set directory that contains labelme annotations and image files
-# labelme_folder = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/train"
-labelme_folder = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/validation"
+# labelme_folder = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/train.json"
+labelme_folder = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/validation.json"
 
 # set path for coco json to be saved
-# save_json_path = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/coco_train"
-save_json_path = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/coco_val"
+# save_json_path = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/coco_train.json"
+save_json_path = "/home/beata/Studia/TP/Change_Your_Background/TrainOwnSegmentationNetwork/Dataset/coco_val.json"
 
 # convert labelme annotations to coco
 labelme2coco.convert(labelme_folder, save_json_path)
